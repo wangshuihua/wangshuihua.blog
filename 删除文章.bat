5122ffb164bbf3e46d4a0d35b4e0140139e4b080
wangshuihua <shuihua.usa@gmail.com>
2015-05-16 15:42:47 +0800

    0.2

--- 删除文章.bat
+++ 删除文章.bat
@@ -13,6 +13,15 @@ call del %delfile%^.md
 set /p choice=是否还要删除其他文章？（yes/no）：
 if "%choice%"=="yes" goto RETRY
 if "%choice%"=="y" goto RETRY
+if "%choice%"=="n" goto GOON
+:GOON
+@echo ============================================
+@echo 重新编译发布中……
+@echo ============================================
+call hexo d -g
+@echo ============================================
+@echo 编译并发布成功！
+@echo ===========================================
 echo 【按任意键退出】
 pause>nul
 exit
